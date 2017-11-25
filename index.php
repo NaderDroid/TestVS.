2b2be7f19dd2a546c53652f411d600367539e4c5
Nader <na511dr@hotmail.com>
2017-11-25 11:22:09 -0500

    its working now

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 This is only for testing pusrposes!
+
+
+<php
+echo "this file is being edited from the vscode editor for testing purposes
+
+only." . " i honestly like this ediotr as its truley powerful and lignhtweight!"
+
+echo "Nader is the best for testing purposes only!!"
+
+?>
+
